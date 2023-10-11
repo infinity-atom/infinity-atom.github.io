a35cc3996259d5ff3f60b683ec9a376f90997e2e
infinity-atom <Leonard.jj@outlook.com>
2023-10-11 15:56:18 +1000

    second mobile test

--- assets/js/-all.js
+++
@@ -1,7 +0,0 @@
-const resizer = () => {
-    document.getElementById("content").style.height = window.innerHeight + "px";
-}
-
-window.addEventListener("resize", (_e) => resizer());
-
-document.addEventListener("DOMContentLoaded", (_e) => resizer());
